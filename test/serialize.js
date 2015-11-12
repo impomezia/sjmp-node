a869524145d563ca31371976eb532e2b6b156ce5
Alexander Sedov <impomezia@gmail.com>
2015-11-12 15:55:02 +0300

    Improved serialize performance.

---
+++ test/serialize.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var Benchmark = require('benchmark');
+var sjmp      = require('../lib/sjmp');
+var suite     = new Benchmark.Suite;
+
+console.log(  sjmp.serialize({
+  type: '<',
+  method: 'get',
+  version: 1,
+  resource: 'some/resource',
+  id: 'MessageId',
+  date: 1395591341000,
+  headers: {},
+  body: 'request body'
+}, false));
+
+
+function test1() {
+  sjmp.serialize({
+    type: '<',
+    method: 'get',
+    version: 1,
+    resource: 'some/resource',
+    id: 'MessageId',
+    date: 1395591341000,
+    headers: {},
+    body: 'request body'
+  }, false)
+}
+
+function test2() {
+  sjmp.serialize({
+    type: '<',
+    method: 'get',
+    version: 1,
+    resource: 'some/resource',
+    id: 'MessageId',
+    date: 1395591341000,
+    headers: {},
+    body: 'request body'
+  }, true)
+}
+
+suite.add('Serialize#Array', test1);
+suite.add('Serialize#JSON', test2);
+
+suite.on('cycle', function(event) {
+  console.log(String(event.target));
+});
+
+suite.on('complete', function() {
+  process.exit(0);
+});
+
+suite.run();
